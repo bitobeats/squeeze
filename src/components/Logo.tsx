d551f7bed9ce9f5800e16188037a649fe262cf1a
Vitor Loureiro <miseravel.cruller-0o@icloud.com>
2022-10-31 04:47:41 -0300

    "Squeeze" logo is now a link to itself

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -3,7 +3,9 @@ import { Typography } from "@mui/material";
 function Logo() {
     return (
         <Typography fontSize={30} fontFamily={'"Baloo 2"'} sx={{ flexGrow: 1 }}>
-            squeeze
+            <a href={window.location.href} style={{ textDecoration: "none", color: "white" }}>
+                squeeze
+            </a>
         </Typography>
     );
 }
