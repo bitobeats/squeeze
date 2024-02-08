b7241ea24bc2807566a0ee081e0f3ebaba80211b
Vitor Loureiro <miseravel.cruller-0o@icloud.com>
2024-02-07 21:41:19 -0300

    code cleanup

--- src/components/ReloadPrompt.tsx
+++ src/components/ReloadPrompt.tsx
@@ -3,72 +3,71 @@ import CloseIcon from "@mui/icons-material/Close";
 import { useRegisterSW } from "virtual:pwa-register/react";
 
 const downloadButtonTheme = createTheme({
-    palette: {
-        primary: {
-            main: "rgba(85, 211, 154, 1)",
-        },
+  palette: {
+    primary: {
+      main: "rgba(85, 211, 154, 1)",
     },
+  },
 });
 
 export default function ReloadPrompt() {
-    const {
-        offlineReady: [offlineReady, setOfflineReady],
-        needRefresh: [needRefresh, setNeedRefresh],
-        updateServiceWorker,
-    } = useRegisterSW({
-        onRegistered() {
-            // eslint-disable-next-line prefer-template
-        },
-        onRegisterError() {},
-    });
+  const {
+    offlineReady: [offlineReady, setOfflineReady],
+    needRefresh: [needRefresh, setNeedRefresh],
+    updateServiceWorker,
+  } = useRegisterSW({
+    onRegistered() {},
+    onRegisterError() {},
+  });
 
-    const handleClick = async () => {
-        await updateServiceWorker(true);
-        handleClose();
-    };
-    const handleClose = () => {
-        setOfflineReady(false);
-        setNeedRefresh(false);
-    };
-    const popupMessage = () => {
-        let message = "";
-        if (offlineReady) {
-            message = "Ready to work offline!";
-        } else {
-            message = "New update available!";
-        }
-        return message;
-    };
+  const handleClick = async () => {
+    await updateServiceWorker(true);
+    handleClose();
+  };
+  const handleClose = () => {
+    setOfflineReady(false);
+    setNeedRefresh(false);
+  };
 
-    const action = (
-        <>
-            {needRefresh && (
-                <ThemeProvider theme={downloadButtonTheme}>
-                    <Button onClick={handleClick}>Install</Button>
-                </ThemeProvider>
-            )}
+  const popupMessage = () => {
+    let message = "";
+    if (offlineReady) {
+      message = "Ready to work offline!";
+    } else {
+      message = "New update available!";
+    }
+    return message;
+  };
 
-            <IconButton onClick={handleClose} sx={{ color: "white" }}>
-                <CloseIcon fontSize="small" />
-            </IconButton>
-        </>
-    );
+  const action = (
+    <>
+      {needRefresh && (
+        <ThemeProvider theme={downloadButtonTheme}>
+          <Button onClick={handleClick}>Install</Button>
+        </ThemeProvider>
+      )}
 
-    return (
-        <Snackbar
-            open={offlineReady || needRefresh}
-            message={needRefresh ? popupMessage() : null}
-            autoHideDuration={null}
-            action={action}
-            onClose={handleClose}
-            sx={{
-                marginBottom: "env(safe-area-inset-bottom)",
-            }}>
-            {offlineReady ? (
-                <Alert onClose={handleClose} severity="success">
-                    {popupMessage()}
-                </Alert>
-            ) : undefined}
-        </Snackbar>
-    );
+      <IconButton onClick={handleClose} sx={{ color: "white" }}>
+        <CloseIcon fontSize="small" />
+      </IconButton>
+    </>
+  );
+
+  return (
+    <Snackbar
+      open={offlineReady || needRefresh}
+      message={needRefresh ? popupMessage() : null}
+      autoHideDuration={null}
+      action={action}
+      onClose={handleClose}
+      sx={{
+        marginBottom: "env(safe-area-inset-bottom)",
+      }}>
+      {offlineReady ? (
+        <Alert onClose={handleClose} severity="success">
+          {popupMessage()}
+        </Alert>
+      ) : undefined}
+    </Snackbar>
+  );
 }
