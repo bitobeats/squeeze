b1fecaa048e7289075201b9291b2a108116d727a
Vitor Loureiro <miseravel.cruller-0o@icloud.com>
2024-04-16 20:44:40 -0300

    code reformat and cleanup

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,13 +1,13 @@
 import { Typography } from "@mui/material";
 
 function Logo() {
-    return (
-        <Typography fontSize={30} fontFamily={'"Baloo 2"'} sx={{ flexGrow: 1 }}>
-            <a href={window.location.href} style={{ textDecoration: "none", color: "white" }}>
-                squeeze
-            </a>
-        </Typography>
-    );
+  return (
+    <Typography fontSize={30} fontFamily={'"Baloo 2"'} sx={{ flexGrow: 1 }}>
+      <a href={window.location.href} style={{ textDecoration: "none", color: "white" }}>
+        squeeze
+      </a>
+    </Typography>
+  );
 }
 
 export default Logo;
