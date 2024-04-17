0548d57d073c9ddc772af12893e07b802cfefacf
Vitor Loureiro <miseravel.cruller-0o@icloud.com>
2024-04-16 21:56:04 -0300

    changed font serving logic

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -2,7 +2,7 @@ import { Typography } from "@mui/material";
 
 function Logo() {
   return (
-    <Typography fontSize={30} fontFamily={'"Baloo 2"'} sx={{ flexGrow: 1 }}>
+    <Typography fontSize={30} fontFamily={'"Baloo 2 Variable"'} sx={{ flexGrow: 1 }}>
       <a href={window.location.href} style={{ textDecoration: "none", color: "white" }}>
         squeeze
       </a>
--- src/main.tsx
+++ src/main.tsx
@@ -1,23 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-import '@fontsource/roboto/300.css'
-import '@fontsource/roboto/400.css'
-import '@fontsource/roboto/500.css'
-import '@fontsource/roboto/700.css'
-import '@fontsource/ubuntu/300.css'
-import '@fontsource/ubuntu/400.css'
-import '@fontsource/ubuntu/700.css'
-import '@fontsource/baloo-2/400.css'
+import "@fontsource-variable/baloo-2";
 
+import "./index.css";
 
-import './index.css'
-
-
-
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-)
+);
